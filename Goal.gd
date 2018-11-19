3dea1d7ee4a0abc3b682289e7e05faba68d6939f
Lue <lue@archlinux.local>
2018-11-19 08:56:57 +0100

    Making sure the player is alive at Goal

--- Goal.gd
+++ Goal.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
 onready var scene = "res://Levels/" + str(int(get_tree().get_current_scene().name) + 1) + ".tscn"
-
+onready var player = $"../Player"
 
 func _on_Goal_body_entered(body):
-	get_tree().change_scene(scene)
+	if player.alive:
+		get_tree().change_scene(scene)
