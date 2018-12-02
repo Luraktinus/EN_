0bbf5f4bae031774ed14b147973a95b9ee63c98f
Billik <44937255+Billik@users.noreply.github.com>
2018-12-02 08:10:29 +0100

    Level Menu Redesign

--- Menu/LevelButton.gd
+++ Menu/LevelButton.gd
@@ -3,7 +3,13 @@ extends Button
 var level = 0
 
 func _ready():
-	text = str(level)
+	$Label.text = str(level)
+
 
 func _on_Button1_pressed():
 	get_tree().change_scene("res://Levels/" + str(level) + ".tscn")
+
+
+func set_font(font):
+	print(font)
+	$Label.set("custom_fonts/font", load(font))
--- Menu/LevelMenu.gd
+++ Menu/LevelMenu.gd
@@ -4,12 +4,13 @@ var file = File.new() #file
 var save_path = "user://level.save" #place of the file
 var save_data = {"level": 1} #variable to store data
 var button_res = load("res://Menu/LevelButton.tscn")
+var lvl_reached = 1
 	
 func _ready():
 	if not file.file_exists(save_path):
 		create_save()
-	print("Read savefile: "+str(read_savegame()))
-	var lvl_reached = int(read_savegame())
+	else:
+		lvl_reached = int(read_savegame())
 	var n = 1
 	while file.file_exists("res://Levels/" + str(n) + ".tscn"):
 		n += 1
@@ -19,6 +20,9 @@ func _ready():
 		button.level = i
 		if i > lvl_reached:
 			button.disabled = true
+			button.set_font("res://Menu/MenuFont30.tres")
+		else:
+			button.set_font("res://Menu/MenuFont50.tres")
 		$ScrollContainer/GridContainer.add_child(button)
 		
 
